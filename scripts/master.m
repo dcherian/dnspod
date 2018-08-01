e34416db202ffd894aba548f1cfd6757f40b4724
dcherian <dcherian@mit.edu>
2018-07-31 17:33:23 -0700

    Get things working.

--- scripts/master.m
+++ scripts/master.m
@@ -1,16 +1,20 @@
 addpath(genpath('./'))
 
-simdir = '../simulation_slices_Re1000Ri012Pr1/';
+% name simulation
+simdir = '../slices/simulation_slices_Re1000Ri012Pr1/';
+sim_info.Re = 1000;
+sim_info.Pr = 1;
+sim_info.Ri = 0.12;
 
 % dimensional parameters
 layer.nu = 8e-4;
-layer.kappa = layer.nu/first.sim_info.Pr;
+layer.kappa = layer.nu/sim_info.Pr;
 % 2h* (m)
 layer.width = 10;
 % 2u* (m/s) velocity jump
-layer.vjmp = 2 * first.sim_info.Re * layer.nu / (layer.width/2);
+layer.vjmp = 2 * sim_info.Re * layer.nu / (layer.width/2);
 % 2B* buoyancy jump
-layer.Bjmp = first.sim_info.Ri * (layer.vjmp/2)^2 / (layer.width/2);
+layer.Bjmp = sim_info.Ri * (layer.vjmp/2)^2 / (layer.width/2);
 layer.timescale = layer.width/layer.vjmp;
 layer
 
